3be9d6d6fcc0edd1af13807dcf50e917438dad90
razzaksr <razzaksr@gmail.com>
2025-07-03 16:12:19 +0530

    CRUD using list and localStorage

---
+++ CRUDOperations/crud.js
@@ -0,0 +1,64 @@
+// let students = [];
+let students = JSON.parse(localStorage.getItem("students")) || [];
+
+
+function renderTable() {
+  console.log("Rendering Students:", students);
+
+  const table = document.getElementById("studentTable");
+  table.innerHTML = "";
+
+  students.forEach((student, i) => {
+    table.innerHTML += `
+      <tr>
+        <td>${student.name}</td>
+        <td>${student.roll}</td>
+        <td>${student.dept}</td>
+        <td>
+          <button onclick="editStudent(${i})">Edit</button>
+          <button onclick="deleteStudent(${i})">Delete</button>
+        </td>
+      </tr>`;
+  });
+}
+
+document.getElementById("studentForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const roll = document.getElementById("roll").value;
+  const dept = document.getElementById("dept").value;
+  const index = document.getElementById("index").value;
+
+  const student = { name, roll, dept };
+
+  if (index === "") {
+    students.push(student);
+  } else {
+    students[index] = student;
+    document.getElementById("index").value = "";
+  }
+  localStorage.setItem("students", JSON.stringify(students));
+
+
+  console.log("Updated Students:", students);
+  this.reset();
+  renderTable();
+});
+
+function editStudent(i) {
+  const student = students[i];
+  document.getElementById("name").value = student.name;
+  document.getElementById("roll").value = student.roll;
+  document.getElementById("dept").value = student.dept;
+  document.getElementById("index").value = i;
+}
+
+function deleteStudent(i) {
+  students.splice(i, 2);
+  console.log("After Deletion:", students);
+  localStorage.setItem("students", JSON.stringify(students));
+  renderTable();
+}
+
+renderTable();
